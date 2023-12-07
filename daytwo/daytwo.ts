9f867f33a400d7b5f57cc5bfb76d4332a530b4d1
Radhi Rasho <Radhi.h@outlook.com>
2023-12-07 02:55:58 +0000

    Day 2 Part 2 Finished

--- daytwo/daytwo.ts
+++ daytwo/daytwo.ts
@@ -4,14 +4,14 @@ const text = await file(`${import.meta.dir}/daytwo.txt`).text();
 
 const daytwo = text.split("\n");
 
-const colors: { [key: string]: number } = {
-	red: 12,
-	green: 13,
-	blue: 14,
-};
+type Color = { [key: string]: number };
 
-function parseData(sets: string[] | undefined): boolean {
-	if (!sets) return false;
+function parseData(sets: string[]): number {
+	const colors: Color = {
+		red: 0,
+		green: 0,
+		blue: 0,
+	};
 
 	for (const set of sets) {
 		const cubes = set.split(",").map((x) => x.trim());
@@ -20,31 +20,28 @@ function parseData(sets: string[] | undefined): boolean {
 			const [number, color] = cube.split(" ");
 			const num = parseInt(number);
 
-			if (num > colors[color]) {
-				return false;
-			}
+			colors[color] = Math.max(colors[color], num);
 		}
 	}
 
-	return true;
+	console.log(colors);
+	const max = colors.red * colors.green * colors.blue;
+
+	return max;
 }
 
-let total = 0;
-
-for (const g of daytwo) {
-	const game = g.split(":");
-	const gameID = game.at(0)?.replace(/\D/g, "");
+const total = daytwo.reduce((acc, curr) => {
+	const game = curr.split(":");
 	const sets = game
 		.at(-1)
 		?.split(";")
 		.map((x) => x.trim());
 
-	if (parseData(sets)) {
-		if (!gameID) continue;
+	if (!sets) return acc;
 
-		console.log(`======================== ${gameID} ========================`);
-		total += parseInt(gameID);
-	}
-}
+	const max = parseData(sets);
+
+	return max + acc;
+}, 0);
 
 console.log(total);
